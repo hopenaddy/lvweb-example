da1ba0bb1f4cf77b0c32750f00e5526932bafd82
Vadim A. Byshovets <Vadim A. Byshovets>
2014-10-30 14:43:31 +0200

    Logging added, logging to STDOUT

--- example.py
+++ example.py
@@ -1,12 +1,14 @@
 from twisted.internet import reactor
 from twisted.web import server, resource
-
+from twisted.python import log
+import sys
 
 class Hello(resource.Resource):
     isLeaf = True
     def render_GET(self, request):
         return "Hello LVWEB-128!"
 
+log.startLogging(sys.stdout)
 
 site = server.Site(Hello())
 reactor.listenTCP(8888, site)
